dd40707def85ca73ca5e059810a752896727be9e
Peak <92136161+Peak1260@users.noreply.github.com>
2025-02-04 18:38:06 -0800

    Update i2c.h

--- peripheral_uart/src/i2c.h
+++ peripheral_uart/src/i2c.h
@@ -4,13 +4,23 @@
 #include <zephyr/device.h>
 #include <stdint.h>
 #include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/i2c.h>
+
+#define REPORT_SUCCESS false
+
 // General I2C read/write functions
 int i2c_write_register(const struct device *i2c_dev, uint8_t dev_addr, uint8_t reg_addr, uint8_t data);
 int i2c_read_register(const struct device *i2c_dev, uint8_t dev_addr, uint8_t reg_addr, uint8_t *data);
 int i2c_read_registers(const struct device *i2c_dev, uint8_t dev_addr, uint8_t reg_addr, uint8_t *data, size_t len);
 void i2c_init(void);
-void i2c_read_data();
+void i2c_read_data(void);
+
+bool d_i2c_is_ready(const struct i2c_dt_spec *i2c_dev);
+bool d_i2c_write_byte(const struct i2c_dt_spec *i2c_dev, uint8_t address, uint8_t data);
+bool d_i2c_read_register(const struct i2c_dt_spec *i2c_dev, uint8_t reg_addr, uint8_t *data);
+bool d_i2c_read_registers(const struct i2c_dt_spec *i2c_dev, uint8_t reg_addr, uint8_t *data, size_t len);
 
 #endif
